a77c351b1632aac5ec863475d2a61ced9a59331e
Altay Zhenis <Altayaka47@gmail.com>
2020-03-26 21:05:30 +0600

    Добавьте файлы проекта.

---
+++ BeckyShop/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeckyShop.Models;
+using BeckyShop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BeckyShop.Controllers
+{
+    public class PieController : Controller
+    {
+        // GET: /<controller>/
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieList = new PieListViewModel();
+            pieList.ListOfPies = _pieRepository.AllPies;
+            pieList.CurrentCategory = "Cheese cakes";
+
+            return View(pieList);
+        }
+    }
+}
---
+++ BeckyShop/Models/ICategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeckyShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
